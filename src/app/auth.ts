92d98ec03454e13533247ec5bb0cc8e03cc6f57f
SahilDahekar <dahekar30sahil@gmail.com>
2025-08-06 20:07:44 +0530

    Initial commit

---
+++ src/app/auth.ts
@@ -0,0 +1,41 @@
+import { createClient } from "@openauthjs/openauth/client"
+import { cookies as getCookies } from "next/headers"
+
+export const client = createClient({
+  clientID: "test_client_nextjs",
+  issuer: "http://127.0.0.1:8787",
+})
+
+import { createSubjects } from "@openauthjs/openauth/subject";
+import { email, object, pipe, string, url } from "valibot";
+
+
+export const subjects = createSubjects({
+  user: object({
+    id: string(),
+    name: string(),
+    email: pipe(string(), email()),
+    picture: pipe(string(), url())
+  }),
+});
+
+export async function setTokens(access: string, refresh: string) {
+  const cookies = await getCookies()
+
+  cookies.set({
+    name: "access_token",
+    value: access,
+    httpOnly: true,
+    sameSite: "lax",
+    path: "/",
+    maxAge: 34560000,
+  })
+  cookies.set({
+    name: "refresh_token",
+    value: refresh,
+    httpOnly: true,
+    sameSite: "lax",
+    path: "/",
+    maxAge: 34560000,
+  })
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,52 @@
-import Image from "next/image";
+import Image from "next/image"
+import { auth, login, logout } from "./actions"
 
-export default function Home() {
+export default async function Home() {
+  const subject = await auth()
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="">
+      <main className="p-4">
+        <div className="p-4">
+          {subject ? (
+            <div>
+              <p className="my-2">
+                Logged in as <code>{subject.properties.name}</code>.
+              </p>
+              <div className="flex items-center gap-4">
+                <Image height={48} width={48} alt="profile picture" className="rounded-full" src={subject.properties.picture}/>
+                <div className="flex flex-col">
+                  <code className="font-semibold text-xl">{subject.properties.name}</code>
+                  <code className="text-sm text-slate-400">{subject.properties.email}</code>
+                </div>
+              </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+              
+            </div>
+          ) : (
+            <>
+              <p>No Logged in User</p>
+            </>
+          )}
+        </div>
+
+        <hr />
+
+        <div className="p-4">
+          {subject ? (
+            <form action={logout}>
+              <button className="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150 cursor-pointer">Logout</button>
+            </form>
+          ) : (
+            <form action={login}>
+                <button className="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150 cursor-pointer">
+                    <Image height={6} width={6} className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo"/>
+                    <span>Login with Google</span>
+                </button>
+            </form>
+          )}
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
-  );
+  )
 }
